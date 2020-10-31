d26aa944229ec04d8f380fa187c16cd220254772
Alex Caulfield <ghost2012qte@yandex.ru>
2020-10-31 07:51:52 +0300

    Intents all

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,15 @@
-import { Client } from 'discord.js';
+import { Client, Intents } from 'discord.js';
 import { LocalStorage } from "node-localstorage";
 import { RoleManager } from './managers/role-manager';
 import { BotManager } from './managers/bot-manager';
 
-const bot = new Client();
+const intents = new Intents([
+    Intents.ALL
+    // Intents.NON_PRIVILEGED, // include all non-privileged intents, would be better to specify which ones you actually need
+    // "GUILD_PRESENCES",
+    // "GUILD_MEMBERS" // lets you request guild members (i.e. fixes the issue)
+])
+const bot = new Client({ws: {intents}});
 
 export default bot;
 
